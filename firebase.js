afa11ca35e84fc6942ceb869bfa226216a212da8
MatthewHeidemann <matt.heidemann@gmail.com>
2025-05-06 14:00:51 -0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCAboWHmvgh8Q88kYYP2UHk3AVokmCz_xc",
+  authDomain: "sdtrcmondaynightdoubles.firebaseapp.com",
+  projectId: "sdtrcmondaynightdoubles",
+  storageBucket: "sdtrcmondaynightdoubles.firebasestorage.app",
+  messagingSenderId: "85676299683",
+  appId: "1:85676299683:web:3025b5fee1fda95b607616",
+  measurementId: "G-SNXPWZ08F9",
+  databaseURL: "https://sdtrcmondaynightdoubles-default-rtdb.firebaseio.com"
+};
+
+firebase.initializeApp(firebaseConfig);
